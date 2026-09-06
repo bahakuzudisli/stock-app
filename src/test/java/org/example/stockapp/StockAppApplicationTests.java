dbf1c40ea23e1e4155df043f5dd3fb0d6ad18971
baha06tr <baha71434@gmail.com>
2026-09-06 16:18:31 +0300

    Initial commit

---
+++ src/test/java/org/example/stockapp/StockAppApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.stockapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
